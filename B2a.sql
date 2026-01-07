3f2dcad9bab562fd4c4f0d741983ec90033b1bda
CHENSHEN04 <159114026+CHENSHEN04@users.noreply.github.com>
2026-01-07 19:09:52 +0800

    Update B2a.sql

--- B2a.sql
+++ B2a.sql
@@ -23,20 +23,3 @@ BEGIN
   v_diff := v_end - v_start;
   DBMS_OUTPUT.PUT_LINE('Insert time: ' || v_diff);
 END;
-
-b)	Find update query time in oracle apex
-DECLARE
-  v_start TIMESTAMP;
-  v_end   TIMESTAMP;
-  v_diff  INTERVAL DAY TO SECOND;
-BEGIN
-  v_start := SYSTIMESTAMP;
-
-  UPDATE STRESSLEVEL
-  SET STRESS_LEVEL = 4
-  WHERE MOD(ID, 10) = 0;
-
-  v_end := SYSTIMESTAMP;
-  v_diff := v_end - v_start;
-  DBMS_OUTPUT.PUT_LINE('Update time: ' || v_diff);
-END;
