b12404df812f358d4667cbf6a94581c636d61e72
CHENSHEN04 <159114026+CHENSHEN04@users.noreply.github.com>
2026-01-07 19:02:00 +0800

    Scalability
    Clone to 10k in Oracle Apex
    Find update query time in oracle apex

---
+++ B1b.sql
@@ -0,0 +1,15 @@
+DECLARE
+  v_start TIMESTAMP;
+  v_end   TIMESTAMP;
+  v_diff  INTERVAL DAY TO SECOND;
+BEGIN
+  v_start := SYSTIMESTAMP;
+
+  UPDATE STRESSLEVEL
+  SET STRESS_LEVEL = 5
+  WHERE MOD(ID, 10) = 0;
+
+  v_end := SYSTIMESTAMP;
+  v_diff := v_end - v_start;
+  DBMS_OUTPUT.PUT_LINE('Update time: ' || v_diff);
+END;
