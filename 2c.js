3b639a50b4c48a464cab58f023f8ca111169516a
CHENSHEN04 <159114026+CHENSHEN04@users.noreply.github.com>
2026-01-07 21:57:42 +0800

    Update 2c.js

--- 2c.js
+++ 2c.js
@@ -4,3 +4,11 @@ db.StressRecords.deleteOne({ _id: 2 });
 
 let end = Date.now();
 print("Delete execution time: " + (end - start) + " ms");
+
+let start = Date.now();
+
+let result = db.StressRecords.findOne({ _id: 1 });
+
+let end = Date.now();
+print("Select execution time: " + (end - start) + " ms");
+printjson(result);
