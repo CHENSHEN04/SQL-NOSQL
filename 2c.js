675ec14fcf79e8499c441fa9f287a3f00ef34413
CHENSHEN04 <159114026+CHENSHEN04@users.noreply.github.com>
2026-01-07 21:58:09 +0800

    Performance Benchmarks and Query Speed
    StressLevel Dataset in MongoDB (1k)
    Find delete query time

--- 2c.js
+++ 2c.js
@@ -5,10 +5,3 @@ db.StressRecords.deleteOne({ _id: 2 });
 let end = Date.now();
 print("Delete execution time: " + (end - start) + " ms");
 
-let start = Date.now();
-
-let result = db.StressRecords.findOne({ _id: 1 });
-
-let end = Date.now();
-print("Select execution time: " + (end - start) + " ms");
-printjson(result);
